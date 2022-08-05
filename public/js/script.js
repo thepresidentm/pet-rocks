5972eddce1b9fa143ae80ab1e8a113ac824688b8
thepresidentm <miguelanva9@gmail.com>
2022-08-04 21:00:20 -0500

    victoria y derrota basicos

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,10 @@ let btnPapel = document.getElementById("papel");
 let btnTijeras = document.getElementById("tijeras");
 let txtEspera = document.getElementById("wait");
 
+let eleccion = "piedra";
+
+// Falta corregir que se esperen para llegar al resultado
+
 btnPiedra.addEventListener("click", () =>{
     enviar("piedra");
 });
@@ -19,6 +23,7 @@ socket.on("juego:recibir", (data) => {recibir(data)});
 
 function enviar(x){
     socket.emit("juego:enviar", x);
+    eleccion = x;
     btnPapel.style.display = "none";
     btnPiedra.style.display = "none";
     btnTijeras.style.display = "none";
@@ -26,7 +31,35 @@ function enviar(x){
 }
 
 function recibir(data){
-    window.alert(data);
+    let victoria = 1;
+    // 0 = derrota
+    // 1 = victoria
+    // 2 = empate
+    if(data != "corrupto"){
+        if(eleccion == "piedra"){
+            victoria =
+            data == "papel" ? 0 :
+            data == "tijeras" ? 1 :
+            2
+        }
+        else if(eleccion == "papel"){
+            victoria =
+            data == "tijeras" ? 0 :
+            data == "piedra" ? 1 :
+            2
+        }
+        else if(eleccion == "tijeras"){
+            victoria =
+            data == "piedra" ? 0 :
+            data == "papel" ? 1 :
+            2
+        }
+    }
+    window.alert(
+        victoria == 0 ? "Has perdido" :
+        victoria == 1 ? "Has ganado" :
+        "Has empatado"
+    );
     btnPapel.style.display = "inline";
     btnPiedra.style.display = "inline";
     btnTijeras.style.display = "inline";
