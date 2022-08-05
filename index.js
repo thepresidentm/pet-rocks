3131b7232f5285ddab2d8d040697f391671ce41a
thepresidentm <miguelanva9@gmail.com>
2022-08-04 19:50:08 -0500

    server init

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const express = require("express");
+const socketIo = require("socket.io");
+
+// Config server
+const app = express();
+const port = process.env.PORT || "5000";
+app.use(express.static('./public'));
+
+const server = app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
+
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html");
+});
+
+const io = socketIo(server); 
